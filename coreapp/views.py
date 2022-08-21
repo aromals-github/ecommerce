c621d88c617376c1e4c5c70ca6dce2a7c6d65d7d
Aromal S <93247039+aromals-github@users.noreply.github.com>
2022-08-21 22:40:42 +0530

    added templates and a few css

--- coreapp/views.py
+++ coreapp/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'coreapp/index.html')
+
+def shop(request):
+    return render(request, 'coreapp/shop.html')
+
+def about(request):
+    return render (request,'coreapp/about.html')
+
+def contact(request):
+    return render (request,'coreapp/contact.html')
+
