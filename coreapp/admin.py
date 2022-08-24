da7cb1b2a156d00d820f12e2c65e3bad219c65dc
Aromal S <93247039+aromals-github@users.noreply.github.com>
2022-08-24 17:01:23 +0530

    added models smartphones and smart watches

--- coreapp/admin.py
+++ coreapp/admin.py
@@ -1,1 +1,6 @@
 from django.contrib import admin
+from .models import Smart_phone,Smart_watch
+
+
+admin.site.register(Smart_phone)
+admin.site.register(Smart_watch)
--- coreapp/models.py
+++ coreapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Smart_phone(models.Model):
+    brand = models.CharField( max_length=30, null=False)
+    phone_name = models.CharField( max_length=50, null=False)
+    phone_colour = models.CharField( max_length=10, null= True)
+    phone_images = models.ImageField( upload_to="phone_image",blank=True)
+    storage = models.IntegerField()
+    avalablity = models.BooleanField(default=True)
+
+class Smart_watch(models.Model):
+    brand = models.CharField( max_length=20, null=False)
+    name = models.CharField(max_length=30,null= False)
+    watch_colour = models.CharField( max_length=10, null= True)
+    watch_images = models.ImageField( upload_to="watch_image",blank=True)
+    size = models.IntegerField()
+    avalablity = models.BooleanField(default=True)
