22a4c31350f539c6149fe04ca6b941c0a5174645
Aromal S <93247039+aromals-github@users.noreply.github.com>
2022-09-14 09:50:39 +0530

    added

--- coreapp/urls.py
+++ coreapp/urls.py
@@ -5,7 +5,6 @@ from . import views
 
 urlpatterns =[
     path ('' ,views.home , name = "home"),
-    path ('contact/',views.contact , name = "contact"),
     path ('about/',views.about , name = "about"),
     path ('shop/', views.shop , name = "shop"),
     path ('login/', views.login, name = "login"),
