0186ea6a2a2e6491dbf6ce10acefa6b14e081865
Aromal S <93247039+aromals-github@users.noreply.github.com>
2022-10-13 15:03:55 +0530

    models.py added a new model which will work with cart acceptance

--- customer_purchase/models.py
+++ customer_purchase/models.py
@@ -1,12 +1,20 @@
-from datetime import datetime
 from django.db import models
-from django.forms import CharField
 from coreapp.models import User,Smart_phone,Smart_watch,Tabs
 
 
 # Create your models here.
 
 class Cart(models.Model):
-    user = models.ForeignKey(User,on_delete = models.CASCADE)
+    user = models.ForeignKey(User,on_delete = models.CASCADE,null=True)
+    total_cost = models.IntegerField(null=True,blank=True)
+
+    def __char__(self):
+        return self.user
+
+class CartProducts(models.Model):
+    user = models.ForeignKey(Cart,on_delete=models.SET_NULL,null=True)
+    products_name = models.ForeignKey(Smart_phone,on_delete = models.CASCADE,related_name='cartproducts',null=True)
     products = models.CharField(null=True,max_length = 90000)
     
+    def __char__(self):
+        return self.user
